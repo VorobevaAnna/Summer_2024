22ba25204044da3c987a7b3fe49a0c42a20c4dda
VorobevaAnna <anyv0@yandex.ru>
2024-04-23 11:33:19 +0300

    Task1_3.111111

--- Task1_3.py
+++ Task1_3.py
@@ -1,6 +1,7 @@
+#Задача 1_3
 x = float(input())
 y = float(input())
 list = [x+y,x-y,x/y,x*y,x//y]
 print(list)
 sort_num = sorted(list, reverse=True)
-print(f"Воторое по величине число: {sort_num[1]}")
+print(f"Второе по величине число: {sort_num[1]}")
